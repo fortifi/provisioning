cf53ccd28d263453d8be238cc8a705e3d67016e6
Brooke Bryan <brooke@bajb.net>
2017-11-09 09:55:48 +0000

    Add log source

--- src/LogMessage.php
+++ src/LogMessage.php
@@ -8,6 +8,10 @@ class LogMessage
   const WARNING = 'warning';
   const ERROR = 'error';
 
+  const SOURCE_STATUS = 'status';
+  const SOURCE_PROCESS = 'process';
+  const SOURCE_LOG = 'log';
+
   public $timestamp;
   public $type;
   public $message;
